e2079fef34e98a43fa96cd532fc1e0eaa750cc45
rsalihova8922-coder <rsalihova8922@gmail.com>
2026-07-05 20:30:06 +0500

    U script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,62 @@
+/* ======================================================
+   NEXORA LEARN
+   Version: 1.0 MVP
+====================================================== */
 
+console.log("🚀 Nexora Learn іске қосылды!");
+
+const APP = {
+
+    name: "Nexora Learn",
+
+    version: "1.0",
+
+    developer: "Raushan Salikhova",
+
+    ai: "NEXO"
+
+};
+
+// ----------------------
+// Navigation
+// ----------------------
+
+function openStudent(){
+
+    window.location.href="student.html";
+
+}
+
+function openTeacher(){
+
+    window.location.href="teacher.html";
+
+}
+
+// ----------------------
+// NEXO
+// ----------------------
+
+function welcome(){
+
+    console.log("Сәлем! Мен NEXO AI 🤖");
+
+}
+
+welcome();
+
+// ----------------------
+// Future Modules
+// ----------------------
+
+const Lessons=[];
+
+const Tasks=[];
+
+const Progress=[];
+
+const Achievements=[];
+
+const ChatHistory=[];
+
+console.log(APP);
